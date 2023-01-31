4cfc41c5a72ac31f50c0b0a0db5ebd9490e1cde8
Gleyson Lopes <97358393+gleysonlopespro@users.noreply.github.com>
2023-01-31 02:58:16 +0000

    final codespace

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Button.module.css';
-
-interface ButtonProps{
-    background?: 'primary' | 'secundary' | 'danger' | 'sucess';
-}
-
-export function Button(props: ButtonProps){
-    return <button className={styles.button}>Enviar</button>
-}
